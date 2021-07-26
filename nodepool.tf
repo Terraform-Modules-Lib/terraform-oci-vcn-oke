6ad01210e6209c15d0edf514487115e9bc322cc3
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 20:16:03 +0200

    Update nodepool.tf

--- nodepool.tf
+++ nodepool.tf
@@ -33,7 +33,48 @@ locals {
               dst_port = 22
             }
           }
+        )
 
+        egress = merge(         
+          { for net in try(local.subnets.nodepools, []):
+              format("%s_to_%s", try(net.name, net.cidr), try(net.name, net.cidr)) => {
+                description = "Allow pods on one worker node to communicate with pods on other worker nodes."
+                destination = net.cidr
+              }
+          },
+          { path_discovery = {
+              description = "Path discovery."
+              destination = "0.0.0.0/0"
+              protocol = "icmp"
+              type = 3
+              code = 4
+            }
+          },
+          { oci_services = {
+              description = "Allow nodes to communicate with OKE."
+              destination = "0.0.0.0/0"
+              protocol = "tcp"
+            }
+          },
+          { workers_to_endpoint = {
+              description = "Kubernetes worker to Kubernetes API endpoint communication."
+              destination = local.subnets.endpoint.cidr
+              protocol = "tcp"
+              dst_port = 6443
+            }
+          },
+          { workers_to_controlplane = {
+              description = "Kubernetes worker to control plane communication."
+              destination = local.subnets.endpoint.cidr
+              protocol = "tcp"
+              dst_port = 12250
+            }
+          },
+          { workers_to_internet = {
+              description = "(optional) Allow worker nodes to communicate with internet."
+              destination = "0.0.0.0/0"
+            }
+          }
         )
       }
     }
