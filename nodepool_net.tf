2b07c39d3c343962d3e06441f6cf6c57cc1b91b4
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 17:39:01 +0200

    Create nodepool_net.tf

---
+++ nodepool_net.tf
@@ -0,0 +1,9 @@
+locals {
+  nodepool_nets = [ for net in try(local.subnets.nodepools, []):
+      {
+        name = try(net.name, net.cidr)
+        cidr = net.cidr
+        public = try(net.public, false)
+      }
+  ]
+}
