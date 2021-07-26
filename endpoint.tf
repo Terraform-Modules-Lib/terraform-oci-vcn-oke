e6e2c5aa548056d24b7916308b77bafa54c3abf7
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 19:09:45 +0200

    Update endpoint.tf

--- endpoint.tf
+++ endpoint.tf
@@ -4,35 +4,37 @@ locals {
     cidr = local.subnets.endpoint.cidr
     public = try(local.subnets.endpoint.public, false)
     acl = {
-      ingress = {
-        workers_to_endpoint = { for net in try(local.subnets.nodepools, []):
-          format("%s_to_endpoint", try(net.name, net.cidr)) => {
-            description = "Kubernetes worker to Kubernetes API endpoint communication."
-            source = net.cidr
-            protocol = "tcp"
-            dst_port = 6443
-          }
+      ingress = merge(
+        { for net in try(local.subnets.nodepools, []):
+            format("%s_to_endpoint", try(net.name, net.cidr)) => {
+              description = "Kubernetes worker to Kubernetes API endpoint communication."
+              source = net.cidr
+              protocol = "tcp"
+              dst_port = 6443
+            }
+        },
+        { for net in try(local.subnets.nodepools, []):
+            format("%s_to_controlplane", try(net.name, net.cidr)) => {
+              description = "Kubernetes worker to control plane communication."
+              source = net.cidr
+              protocol = "tcp"
+              dst_port = 12250
+            }
+        },
+        { for net in try(local.subnets.nodepools, []):
+            format("%s_path_discovery", try(net.name, net.cidr)) => {
+              description = "Path discovery."
+              source = net.cidr
+              protocol = "icmp"
+              type = 3
+              code = 4
+            }
         }
+      )
 
-        workers_to_controlplane = { for net in try(local.subnets.nodepools, []):
-          format("%s_to_controlplane", try(net.name, net.cidr)) => {
-            description = "Kubernetes worker to control plane communication."
-            source = net.cidr
-            protocol = "tcp"
-            dst_port = 12250
-          }
-        }
-    
-        path_discovery = { for net in try(local.subnets.nodepools, []):
-          format("%s_path_discovery", try(net.name, net.cidr)) => {
-            description = "Path discovery."
-            source = net.cidr
-            protocol = "icmp"
-            type = 3
-            code = 4
-          }
-        }
-      }
+      egress = merge({}, {})
     }
   }
+  
+  
 }
