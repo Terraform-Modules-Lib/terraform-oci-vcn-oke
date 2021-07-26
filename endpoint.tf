58e8957b8f8a89815c0c77877a110c075bcf5e00
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 17:56:38 +0200

    Update endpoint.tf

--- endpoint.tf
+++ endpoint.tf
@@ -3,6 +3,18 @@ locals {
     name = try(local.subnets.endpoint.name, "endpoint")
     cidr = local.subnets.endpoint.cidr
     public = try(local.subnets.endpoint.public, false)
-    acl = {}
+    acl = local.acl_workers_to_endpoint
   }
+  
+  
+  acl_workers_to_endpoint = { for net in try(local.subnets.nodepools, []):
+    format("%s_to_endpoint", try(net.name, net.cidr)) => {
+      description = "Kubernetes worker to Kubernetes API endpoint communication."
+      source = net.cidr
+      protocol = "tcp"
+      dst_port = 6443
+    }
+  }
+    
+    
 }
